4532599841e1455615563afe0a4309783bd2161a
victorbrevig <victorbrevig@gmail.com>
2024-10-01 16:40:03 +0200

    clean up

--- dist/abis/chainlinkOracle.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.chainlinkOracleAbi = void 0;
-exports.chainlinkOracleAbi = [{ "type": "constructor", "inputs": [{ "name": "_ethUsdPriceFeed", "type": "address", "internalType": "address" }], "stateMutability": "nonpayable" }, { "type": "function", "name": "getPrice", "inputs": [{ "name": "_token", "type": "address", "internalType": "address" }, { "name": "_data", "type": "bytes", "internalType": "bytes" }], "outputs": [{ "name": "", "type": "uint256", "internalType": "uint256" }, { "name": "", "type": "bool", "internalType": "bool" }], "stateMutability": "view" }];
